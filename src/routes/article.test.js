86b2dd0b95bffe9d49720e3a8166147f1bc8ccee
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-15 21:58:41 +0200

    Create tests for endpoints

--- src/routes/article.test.js
+++ src/routes/article.test.js
@@ -3,26 +3,37 @@ const server = require('../server');
 
 const ARTICLES_API_URL = '/api/articles';
 
+const article = { title: 'title', text: 'text', img: 'img', category: 'category' };
+const timestamp = 111145454;
+
 describe('ARTICLES ROUTER', () => {
-  describe('POST ROUTE /LOGIN', () => {
+  describe('POST ROUTE /', () => {
     it('should return 200 on success', async () => {
-      const res = await request(server).get(ARTICLES_API_URL);
+      const res = await request(server)
+        .post(ARTICLES_API_URL + '/')
+        .send({ timestamp });
       expect(res.status).toEqual(200);
     });
 
     it('should return 404 on fail (no user found or deleted)', async () => {
-      const res = await request(server).get(ARTICLES_API_URL);
-      expect(res.status).toEqual(200);
+      const res = await request(server)
+        .post(ARTICLES_API_URL + '/')
+        .send({ timestamp });
+      expect(res.status).toEqual(404);
     });
 
-    it('should return a token on success', async () => {
-      const res = await request(server).get(ARTICLES_API_URL);
-      expect(res.body).toEqual({ message: 'API OK' });
+    it('should return an article on success', async () => {
+      const res = await request(server)
+        .post(ARTICLES_API_URL + '/')
+        .send({ timestamp });
+      expect(res.body).toEqual({ article });
     });
 
     it('should return a message on fail', async () => {
-      const res = await request(server).get(ARTICLES_API_URL);
-      expect(res.body).toEqual({ message: 'API OK' });
+      const res = await request(server)
+        .post(ARTICLES_API_URL + '/')
+        .send({ timestamp });
+      expect(res.body).toEqual({ message: "Error. Couldn't retrieve articles" });
     });
   });
 });
