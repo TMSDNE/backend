717d845c1ee1f7ad30b1118518bc735c08dcccae
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-16 17:48:10 -0400

    updated auth router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
-// import the other routers
-// & use router.use to mount them
 
 router.route('/login').post(authController.loginUser);
 router.route('/register').post(authController.registerUser);
