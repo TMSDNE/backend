17423a63c6e69accd4243b16a6590464347d5aec
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-17 11:54:56 -0400

    removed the need to be logged in to call articles api

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const articleController = require('../controllers/articleController');
-const authenticate = require('../middleware/authenticate');
 const cache = require('../middleware/redisMiddleware');
 
-router.route('/').post(authenticate, cache, articleController.makeArticle);
+router.route('/').post(cache, articleController.makeArticle);
 
 module.exports = router;
